5bcad0b53e8afdb9a967da06b51358948c335953
lispcat <187922791+lispcat@users.noreply.github.com>
2025-06-03 22:49:49 -0400

    expanded unit test, improved test hook, before revamp let macros and hook (make vec)

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -10,6 +10,7 @@ use std::{
 use filetags::{run_with_config, set_test_hook, Config, Message, Rule};
 use regex::Regex;
 use tempfile::TempDir;
+use walkdir::WalkDir;
 
 fn create_test_env() -> (TempDir, PathBuf) {
     let temp_dir = TempDir::new().expect("failed to create temp dir");
@@ -26,7 +27,7 @@ fn _serialize_config(at_path: &Path, config: Config) -> anyhow::Result<()> {
 }
 
 macro_rules! let_paths {
-    (base = $base_dir:expr, $(($var:ident, $file:expr $(, create = $create:tt)? )),+ $(,)?) => {
+    (base = $base_dir:expr, $(( $var:ident, $file:expr $(, create = $create:tt)? )),+ $(,)?) => {
         $(
             let $var = $base_dir.join($file);
             $(
@@ -43,7 +44,7 @@ macro_rules! let_paths {
 macro_rules! create_dirs {
     ($($dir:expr),+) => {{
         $(
-            fs::create_dir_all($dir)?;
+            fs::create_dir_all($dir.clone()).expect("failed to create dirs");
         )+
     }};
 }
@@ -51,7 +52,15 @@ macro_rules! create_dirs {
 macro_rules! create_files {
     ($($file:expr),+) => {{
         $(
-            fs::File::create($file)?;
+            fs::File::create($file.clone()).expect("failed to create files");
+        )+
+    }};
+}
+
+macro_rules! create_symlinks {
+    ($(($target:expr, $link:expr)),+) => {{
+        $(
+            std::os::unix::fs::symlink($target.clone(), $link.clone()).expect("failed to symlink");
         )+
     }};
 }
@@ -63,7 +72,7 @@ macro_rules! create_tx_rx {
 }
 
 macro_rules! create_config {
-    ($(($name:expr, $(($watch:expr))+, $(($dest:expr))+, $regex:tt)),+ $(,)?) => {
+    ( $(( $name:expr, $(($watch:expr))+, $(($dest:expr))+, $regex:tt )),+ $(,)? ) => {
         Arc::new(Config {
             rules: vec![
                 $(
@@ -71,16 +80,16 @@ macro_rules! create_config {
                         name: $name.into(),
                         watch: vec![
                             $(
-                                $watch
+                                $watch.clone()
                             )+
                         ],
                         dest: vec![
                             $(
-                                $dest
+                                $dest.clone()
                             )+
                         ],
                         regex: vec![
-                            Regex::new($regex)
+                            Regex::new($regex.clone())
                             .expect("failed to create regex")
                         ],
                         ..Rule::default()
@@ -94,8 +103,9 @@ macro_rules! create_config {
 
 #[test]
 fn run_env_test_1() -> anyhow::Result<()> {
-    // create temp testing env
+    // init
     let (_, root) = create_test_env();
+    let (tx, rx) = create_tx_rx!();
 
     // create dirs
     let_paths!(
@@ -103,39 +113,60 @@ fn run_env_test_1() -> anyhow::Result<()> {
         (watch_dir, "watch_dir"),
         (dest_dir, "dest_dir"),
     );
-    create_dirs!(&root, &watch_dir, &dest_dir);
+    create_dirs!(root, watch_dir, dest_dir);
 
     // create files
     let_paths!(
         base = watch_dir,
-        (file1, "_sample1.txt"),
-        (file2, "sample2.txt"),
-        (file2_renamed, "_sample2.txt"),
-        (file3, "sample3.txt"),
+        // expect no action
+        (file1, "file1.txt"),
+        // expect init scan to symlink
+        (file2, "_file2.txt"),
+        // expect test hook to symlink
+        (file3, "file3.txt"),
+        (file3_renamed, "_file3.txt"),
+        // expect init scan to take no action because already symlinked
+        (file4, "_file4.txt"),
+        // expect init scan to delete symlink since broken
+        (file5_no_file, "file5.txt"),
     );
-    create_files!(&file1, &file2, &file3);
+    create_files!(file1, file2, file3, file4);
 
-    // create channel
-    let (tx, rx) = create_tx_rx!();
+    let_paths!(
+        base = dest_dir,
+        (file4_symlink, "_file4.txt"),
+        (file5_broken_symlink, "_file5.txt"),
+    );
+    create_symlinks!(
+        (file4, file4_symlink),
+        (file5_no_file, file5_broken_symlink)
+    );
 
     // define config
     let config = create_config!(("test1", (watch_dir), (dest_dir), "^_.*"));
 
-    // adding test hook
-    let tx_clone = tx.clone();
-    set_test_hook(move || {
-        // rename file2 to file2_renamed
-        fs::rename(file2.clone(), file2_renamed.clone()).expect("failed to rename file");
+    // test hook
+    set_test_hook({
+        let tx_clone = tx.clone();
+        let file3 = file3.clone();
+        let file3_renamed = file3_renamed.clone();
+        move || {
+            // rename file3 to file3_renamed
+            fs::rename(file3.clone(), file3_renamed.clone()).expect("failed to rename file");
 
-        // shutdown
-        thread::sleep(Duration::from_millis(100));
-        tx_clone
-            .send(Message::Shutdown)
-            .expect("failed to shutdown");
+            // shutdown
+            thread::sleep(Duration::from_millis(100));
+            tx_clone
+                .send(Message::Shutdown)
+                .expect("failed to shutdown");
+        }
     });
 
     // start the main process loop
     run_with_config(config, tx, rx)?;
+
+    // verify fs
+    // let file3_symlink = dest_dir.join(file3_renamed.file_name().unwrap());
 
     Ok(())
 }
